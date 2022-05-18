d0dea41a13a7e690b0816fc55a3fa82c04f86764
natferlima <ferlima.natalia@gmail.com>
2022-05-17 23:00:53 -0300

    feat: cria rotas get, put, delete

--- backend/routes/task.js
+++ backend/routes/task.js
@@ -3,9 +3,12 @@ const { taskController } = require('../controllers');
 
 const router = express.Router();
 
+const routeWithId = '/tasks/:id';
+
 router.get('/tasks', taskController.readAll);
 router.post('/tasks', taskController.create);
-// router.put('/tasks/:id', taskController);
-// router.delete('/tasks/:id', taskController);
+router.get(routeWithId, taskController.readOne);
+router.put(routeWithId, taskController.update);
+router.delete(routeWithId, taskController.remove);
 
 module.exports = router;
