7540cf727254388bcae71c9111d2c97bd70c36b8
natferlima <ferlima.natalia@gmail.com>
2022-05-18 16:46:41 -0300

    feat: adc middlewares e schemas para validação

--- backend/routes/task.js
+++ backend/routes/task.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const { taskController } = require('../controllers');
+const { fieldsValidation, idValidation } = require('../middlewares');
+const { tasks } = require('../schemas');
 
 const router = express.Router();
 
 const routeWithId = '/tasks/:id';
 
 router.get('/tasks', taskController.readAll);
-router.post('/tasks', taskController.create);
-router.get(routeWithId, taskController.readOne);
-router.put(routeWithId, taskController.update);
-router.delete(routeWithId, taskController.remove);
+router.post('/tasks', fieldsValidation(tasks), taskController.create);
+router.get(routeWithId, idValidation, taskController.readOne);
+router.put(routeWithId, idValidation, taskController.update);
+router.delete(routeWithId, idValidation, taskController.remove);
 
 module.exports = router;
