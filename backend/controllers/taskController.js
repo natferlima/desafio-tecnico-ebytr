7fc55aff921f38e60db2eb027cce64e98a345c39
natferlima <ferlima.natalia@gmail.com>
2022-05-17 22:55:18 -0300

    feat: adc readOne, update e remove no controller

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -11,4 +11,23 @@ const create = async (req, res) => {
   return res.status(201).json(result);
 };
 
-module.exports = { readAll, create };
+const readOne = async (req, res) => {
+  const { id } = req.params;
+  const result = await task.readOne(id);
+  return res.status(200).json(result);
+};
+
+const update = async (req, res) => {
+  const { id } = req.params;
+  const obj = req.body;
+  const result = await task.update(id, obj);
+  return res.status(200).json(result);
+};
+
+const remove = async (req, res) => {
+  const { id } = req.params;
+  const result = await task.remove(id);
+  return res.status(200).json(result);
+};
+
+module.exports = { readAll, create, readOne, update, remove };
