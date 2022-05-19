e5f631f4b6907fd6d043c02c9b4cc47607fbbaa5
natferlima <ferlima.natalia@gmail.com>
2022-05-19 15:47:07 -0300

    feat: adc estado global tasks, setTasks, função getTasks com mock

--- frontend/src/context/GlobalProvider.jsx
+++ frontend/src/context/GlobalProvider.jsx
@@ -1,9 +1,48 @@
-import React from 'react';
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+import GlobalContext from './GlobalContext';
 
-function GlobalProvider() {
+function GlobalProvider({ children }) {
+  const [tasks, setTasks] = useState([]);
+
+  const getTasks = () => {
+    const tasksMock = [
+      {
+        _id: '62842f665f799c435643464e',
+        description: 'estudar modulo 32.1',
+        status: 'pendente',
+        date: '1970-01-01T04:44:12.022Z',
+      },
+      {
+        _id: '62942f665f799c435643464e',
+        description: 'estudar modulo 33.1',
+        status: 'pendente',
+        date: '1970-01-01T04:44:12.022Z',
+      },
+      {
+        _id: '63042f665f799c435643464e',
+        description: 'estudar modulo 34.1',
+        status: 'pendente',
+        date: '1970-01-01T04:44:12.022Z',
+      }
+    ]
+    setTasks(tasksMock);
+  }
+
+  const context = {
+    tasks,
+    getTasks,
+  };
+
   return (
-    <div>GlobalProvider</div>
-  )
+    <GlobalContext.Provider value={ context }>
+      {children}
+    </GlobalContext.Provider>
+  );
 }
+
+GlobalProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 export default GlobalProvider;
