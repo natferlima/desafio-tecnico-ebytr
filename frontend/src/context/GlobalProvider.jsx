72b1a575479c2209697c0c88a365a13447a36c7e
natferlima <ferlima.natalia@gmail.com>
2022-05-19 17:01:38 -0300

    feat: adc axios e faz conexão com a api backend

--- frontend/src/context/GlobalProvider.jsx
+++ frontend/src/context/GlobalProvider.jsx
@@ -1,32 +1,22 @@
 import PropTypes from 'prop-types';
 import React, { useState } from 'react';
 import GlobalContext from './GlobalContext';
+import axios from 'axios';
 
 function GlobalProvider({ children }) {
   const [tasks, setTasks] = useState([]);
 
-  const getTasks = () => {
-    const tasksMock = [
-      {
-        _id: '62842f665f799c435643464e',
-        description: 'estudar modulo 32.1',
-        status: 'pendente',
-        date: '1970-01-01T04:44:12.022Z',
-      },
-      {
-        _id: '62942f665f799c435643464e',
-        description: 'estudar modulo 33.1',
-        status: 'pendente',
-        date: '1970-01-01T04:44:12.022Z',
-      },
-      {
-        _id: '63042f665f799c435643464e',
-        description: 'estudar modulo 34.1',
-        status: 'pendente',
-        date: '1970-01-01T04:44:12.022Z',
-      }
-    ]
-    setTasks(tasksMock);
+  const API = axios.create({
+    baseURL: 'http://localhost:3001',
+  });
+
+  const getTasks = async() => {
+    try{
+      const { data } = await API.get('/tasks');
+      setTasks(data);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   const context = {
