3832355515570f6bbac189ac072aa23113a7a6b3
natferlima <ferlima.natalia@gmail.com>
2022-05-19 16:51:49 -0300

    fix: adc body-parser e cors no backend

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const { connection } = require('./connection');
 const { taskRouter } = require('./routes');
 
 const app = express();
+
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/', taskRouter);
 
